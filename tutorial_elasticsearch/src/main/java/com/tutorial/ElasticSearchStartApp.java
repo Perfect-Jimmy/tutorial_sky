0a2d71eb240276775a1cba81f98764630fcebd45
jimmy <773792867@qq.com>
2020-03-17 15:41:54 +0800

    Initial commit

---
+++ tutorial_elasticsearch/src/main/java/com/tutorial/ElasticSearchStartApp.java
@@ -0,0 +1,17 @@
+package com.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author jimmy
+ * @date 2020/2/16 22:37
+ */
+@SpringBootApplication
+public class ElasticSearchStartApp
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(ElasticSearchStartApp.class,args);
+    }
+}
---
+++ tutorial_springboot/src/main/java/com/tutorial/AppStart.java
@@ -0,0 +1,16 @@
+package com.tutorial;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class AppStart
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
