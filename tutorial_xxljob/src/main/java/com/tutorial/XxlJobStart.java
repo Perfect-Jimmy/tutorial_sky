02e304e6f511375f73bd4391e7f44856ed93b847
zhoucheng <574693497@qq.com>
2020-04-21 11:40:58 +0800

    undertow

---
+++ tutorial_xxljob/src/main/java/com/tutorial/XxlJobStart.java
@@ -0,0 +1,16 @@
+package com.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author jimmy
+ * @date 2020/4/20
+ */
+@SpringBootApplication
+public class XxlJobStart {
+    public static void main(String[] args) {
+        SpringApplication.run(XxlJobStart.class,args);
+    }
+}
